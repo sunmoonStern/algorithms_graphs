203da390bc38e8ac963e506f37ab58607454b531
Mei Sasaki <october.sky.2009@gmail.com>
2019-08-04 23:37:24 +0900

    1/2 passed

--- week1_decomposition1/1_reachability/Reachability.java
+++ week1_decomposition1/1_reachability/Reachability.java
@@ -2,9 +2,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reachability {
-    private static int reach(ArrayList<Integer>[] adj, int x, int y) {
-        //write your code here
-        return 0;
+    private static int cc = 1;
+    private static int reach(ArrayList<Integer>[] adj, int x, int y, int[] connectedComponentsNum) {
+        if (connectedComponentsNum[x] == connectedComponentsNum[y]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static void doDepthFirstSearch(boolean[] visited, int[] connectedComponentsNum, ArrayList<Integer>[] adj) {
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                explore(i, visited, connectedComponentsNum, adj);
+                cc += 1;
+            }
+        }
+    }
+
+    private static void explore(int x, boolean[] visited, int[] connectedComponentsNum, ArrayList<Integer>[] adj) {
+        visited[x] = true;
+        connectedComponentsNum[x] = cc;
+        for (int y: adj[x]) {
+            if (!visited[y]) {
+                explore(y, visited, connectedComponentsNum, adj);
+            }
+        }
     }
 
 
@@ -13,8 +36,12 @@ public class Reachability {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        boolean[] visited = new boolean[n];
+        int[] connectedComponentsNum = new int[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
+            visited[i] = false;
+            connectedComponentsNum[i] = 0;
         }
         for (int i = 0; i < m; i++) {
             int x, y;
@@ -25,7 +52,8 @@ public class Reachability {
         }
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
-        System.out.println(reach(adj, x, y));
+        doDepthFirstSearch(visited, connectedComponentsNum, adj);
+        System.out.println(reach(adj, x, y, connectedComponentsNum));
     }
 }
 
