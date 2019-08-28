f5e1d642a58135d92a0462afe246f007f3a6156c
Mei Sasaki <october.sky.2009@gmail.com>
2019-08-28 23:21:26 +0900

    week 3 passed(2/2)

--- week3_paths1/2_bipartite/Bipartite.java
+++ week3_paths1/2_bipartite/Bipartite.java
@@ -2,11 +2,34 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.ArrayDeque;
 
 public class Bipartite {
     private static int bipartite(ArrayList<Integer>[] adj) {
-        //write your code here
-        return -1;
+        int n = adj.length;
+        int[] dist = new int[n];
+        for (int i = 0; i < n; i++) {
+            dist[i] = n;
+        }
+        Queue<Integer> q = new ArrayDeque<Integer>();
+        int s = 0;
+        dist[s] = 0;
+        q.add(s);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int j = 0; j < adj[u].size(); j++) {
+                int v = adj[u].get(j);
+                if (dist[v] == n) {
+                    q.add(v);
+                    dist[v] = dist[u] + 1;
+                } else {
+                    if ((dist[u] - dist[v]) % 2 == 0) {
+                        return 0;
+                    }
+                }
+            }
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
