ad3dd12651b10b11d934bae7324af7c39e7f98fe
Mei Sasaki <october.sky.2009@gmail.com>
2019-09-15 22:47:02 +0900

    これで2/3 paseed

--- week4_paths2/2_negative_cycle/NegativeCycle.java
+++ week4_paths2/2_negative_cycle/NegativeCycle.java
@@ -2,17 +2,45 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NegativeCycle {
+    static final int MAX_DIST = 10000000;
+    static final int NILL = -1;
     private static int negativeCycle(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost) {
-        // write your code here
-        return 0;
+        int n = adj.length;
+        int[] dist = new int[n];
+        int[] prev = new int[n];
+        for (int i = 0; i < n; i++) {
+            dist[i] = MAX_DIST;
+            prev[i] = NILL;
+        }
+        dist[0] = 0;
+        int indexLastUpdated = -1;
+        for (int i = 0; i <= n; i++) {
+            for (int u = 0; u < n; u++) {
+                for (int j = 0; j < adj[u].size(); j++) {
+                    int v = adj[u].get(j);
+                    if (dist[v] > dist[u] + cost[u].get(j)) {
+                        dist[v] = dist[u] + cost[u].get(j);
+                        prev[v] = u;
+                        if (i == n) {
+                            indexLastUpdated = v;
+                        }
+                    }
+                }
+            }
+        }
+        if (indexLastUpdated == -1) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
+        ArrayList<Integer>[] cost = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
             cost[i] = new ArrayList<Integer>();
