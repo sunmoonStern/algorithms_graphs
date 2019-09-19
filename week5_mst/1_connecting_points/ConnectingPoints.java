18f18175a632c1ab8ac10cdb5da5c4814df63bfd
Mei Sasaki <october.sky.2009@gmail.com>
2019-09-20 00:26:25 +0900

    passed 1/2

--- week5_mst/1_connecting_points/ConnectingPoints.java
+++ week5_mst/1_connecting_points/ConnectingPoints.java
@@ -1,9 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ConnectingPoints {
     private static double minimumDistance(int[] x, int[] y) {
         double result = 0.;
-        //write your code here
+        int n = x.length;
+        double[][] dist = new double[n][n];
+        double[] cost = new double[n];
+        double upperBound = 2. * Math.sqrt(2.) * Math.pow(10., 3);
+        for (int i = 0; i < n; i++) {
+            cost[i] = upperBound;
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = Math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
+            }
+        }
+        cost[0] = 0.;
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            pq.offer(new Point(i, cost[i]));
+        }
+        while (pq.size() > 0) {
+            Point p = pq.poll();
+            int i = p.getIndex();
+            double c = p.getCost();
+            result += c;
+            for (int j = 0; j < n; j++) {
+                if (pq.contains(new Point(j, cost[j])) && cost[j] > dist[i][j]) {
+                    pq.remove(new Point(j, cost[j]));
+                    cost[j] = dist[i][j];
+                    pq.offer(new Point(j, cost[j]));
+                }
+            }
+        }
         return result;
     }
 
@@ -17,6 +44,40 @@ public class ConnectingPoints {
             y[i] = scanner.nextInt();
         }
         System.out.println(minimumDistance(x, y));
+    }
+}
+
+class Point implements Comparable<Point> {
+    private int index;
+    private double cost;
+
+    public Point(int index, double cost) {
+        this.index = index;
+        this.cost = cost;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int compareTo(Point p) {
+        return Double.compare(this.cost, p.getCost());
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Point point = (Point) object;
+        return index == point.index &&
+                java.lang.Double.compare(point.cost, cost) == 0;
+    }
+
+    public int hashCode() {
+        return Objects.hash(index, cost);
     }
 }
 
